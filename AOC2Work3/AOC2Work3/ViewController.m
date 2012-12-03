fd961ce955702c910a8487a59fa5aeacb284f86e
William Saults <wsaults@email.itt-tech.edu>
2012-12-02 20:22:26 -0600

    Added a label and made cell height adjustments.

--- AOC2Work3/AOC2Work3/ViewController.m
+++ AOC2Work3/AOC2Work3/ViewController.m
@@ -51,20 +51,25 @@
         
         id event = [[[EventsManager sharedEventsManager] savedEvents] objectAtIndex:indexPath.row];
         
+        // Title
         UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, cell.frame.size.width, 20)];
         [title setText:[event eventTitle]];
         [title setTextAlignment:NSTextAlignmentCenter];
         [title setFont:[UIFont boldSystemFontOfSize:18]];
         [title setBackgroundColor:[UIColor clearColor]];
         
+        // Description
         UILabel *description = [[UILabel alloc] initWithFrame:CGRectMake(0, 25, cell.frame.size.width, 20)];
         [description setText:[event eventDescription]];
         [description setNumberOfLines:3];
+        [description setBackgroundColor:[UIColor clearColor]];
         
+        // Date
         UILabel *date = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, cell.frame.size.width, 20)];
         NSDateFormatter *dateFormatter = [NSDateFormatter new];
         [dateFormatter setDateFormat:@"EEEE MM/dd/yyyy hh:mm a"];
         [date setText:[dateFormatter stringFromDate:[event eventDate]]];
+        [date setBackgroundColor:[UIColor clearColor]];
         
         [cell addSubview:title];
         [cell addSubview:description];
